5543a98a5e4e265a562c4e27adca390b09815271
Ussiel <ussiel@gmail.com>
2021-06-29 22:18:31 -0500

    primer commit

---
+++ menu/admin.php
@@ -0,0 +1,26 @@
+<?php
+global $MySession;
+return array(
+    array('title'=> "Desarrolladores",
+            'children' =>  array(
+    
+        array(
+            "permiso" =>   ADMINISTRAR_FRANKY,
+            "url" => $MyRequest->url(LISTA_PAGINAS),
+            "etiqueta" => _("Administrar Páginas")
+        ),
+        
+        array(
+            "permiso" =>   ADMINISTRAR_SHELL,
+            "url" => $MyRequest->url(SHELL),
+            "etiqueta" => _("Shell")
+        ),
+        array(
+            "permiso" =>   ADMINISTRAR_FTP,
+            "url" => $MyRequest->url(ADMIN_FTP),
+            "etiqueta" => _("FTP")
+        ),
+    )
+    )
+);
+?>
