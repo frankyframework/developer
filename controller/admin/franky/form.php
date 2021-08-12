61bb5d8792b4c2e8a7508e64e38c519a79b4b143
ussiel <ussiel@gmail.com>
2021-08-11 23:01:38 -0500

    correccion

--- controller/admin/franky/form.php
+++ controller/admin/franky/form.php
@@ -47,68 +47,48 @@ if(count($files) > 0)
 
 $ajax_files = array();
 $modulos = getModulos();
-$js_excluir = array();
+$js_excluir = array("admin.js","front.js");
+$js_files = array();
 foreach($modulos as $modulo)
 {
-    $files = $File->getFiles(PROJECT_DIR."/public/ajax/$modulo/","file");
+    $js_excluir[] = $modulo.".js";
+    $files = $File->getFiles(PROJECT_DIR."/modulos/$modulo/web/js/","file");
 
     if(count($files) > 0)
     {
         foreach($files as $file)
         {
-            $ajax_files["$modulo/".$file] = "$modulo/".$file;
+            if(!in_array($file,$js_excluir))
+            {
+                if(preg_match('/ajax\./',$file))
+                {
+                    $ajax_files["$modulo/".$file] = "$modulo/".$file;
+                }
+                else{
+                    $js_files[$file] = $file;
+                }
+            }
+            
         }
     }
 
-    $js_excluir[] = $modulo.".js";
+   
 }
 
-$js_files = array();
-$files = $File->getFiles(PROJECT_DIR."/public/js/","file");
 
+$css_files = array();
+
+
+$files = $File->getFiles(PROJECT_DIR."/modulos/".$MyConfigure->getPathSite()."/web/css/","file");
 if(count($files) > 0)
 {
     foreach($files as $file)
     {
-        if(!in_array($file,array_merge(array("funciones.js","viewport.js","modernizr.js"),$js_excluir)))
-        {
-            $js_files[$file] = $file;
-        }
+        $css_files[$file] = $file;
     }
 }
 
-$css_files = array();
-$excluir = array("imprimir.css",
-"style.css",
-"grid.css",
-"estilos.css",
-"estilos_320.css",
-"estilos_640.css",
-"estilos_960.css",
-"normalize.css",
-"panel.css",
-"panel_extend.css",
-"franky.mobile.css",
-"variables.css"
-);
-$mymodulos = ['default',$MyConfigure->getPathSite()];
-foreach($mymodulos as $modulo)
-{
-
-    $files = $File->getFiles(PROJECT_DIR."/public/skin/$modulo/css/","file");
-    if(count($files) > 0)
-    {
-        foreach($files as $file)
-        {
-          if(!in_array($file,$excluir)):
-            $css_files[$file] = $file;
-        endif;
-        }
-    }
-}
-
-
-       
+  
 $_modulos = array();
 foreach ($modulos as $m)
 {
